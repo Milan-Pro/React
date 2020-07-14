49c72a9accd4cbff5b87a8942d8fb4c04ade1a0e
Milan Parikh <milan.parikh@dart.biz>
2020-07-14 06:17:39 -0400

    Introduction of component and State component

--- reactbasics/src/App.tsx
+++ reactbasics/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import  { Hello  } from "./Hello";
+import { Products } from  "./Products";
 
 function App() {
+
   return (
-    <div>
-      <Hello />
+    <div className="App">
+      <Products />
     </div>
   );
 }
---
+++ reactbasics/src/Product.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+import "./App.css";
+import { IProduct } from "./common/IProduct";
+
+interface IProductProps {
+    item: IProduct
+}
+
+interface IProductState {
+    data: IProduct;
+}
+
+export class Product extends React.Component<IProductProps, IProductState> {
+    constructor(props: IProductProps) {
+        super(props);
+
+        this.state = {
+            data: props.item
+        };   
+    }
+
+    public render() : JSX.Element {
+        return <div className="product">
+            { this.state.data.id } <br/>
+            { this.state.data.name }  <br/>
+            { this.state.data.cat } <br/>
+            { this.state.data.price } &nbsp;
+
+            <input type="button" onClick={ () => {
+                let item = this.state.data;
+                item.price += 10;
+
+                this.setState({
+                    data: item
+                });
+            }} value=" + " />
+        </div>;
+    }
+}
